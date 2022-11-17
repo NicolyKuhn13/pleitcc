aefc32aae5a62ce415a6e39510630a4143001338
NicolyKuhn13 <nicolycolorsk@gmail.com>
2022-11-16 22:25:57 -0300

    reset e inicialização do arquivo login.blade

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $nome = "Nicoly";
+    $idade = 18;
+    return view('welcome', ['nome' => $nome, 'idade' => $idade]);
+});
+
+Route::get('/login', function () {
+
+    return view('login');
 });
