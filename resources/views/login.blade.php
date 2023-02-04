9433d87a6365806f206327ffc972b4ec9e1dd4ac
NicolyKuhn13 <nicolycolorsk@gmail.com>
2023-02-04 16:55:14 -0300

    changes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,22 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
-</head>
+@extends('layouts.main')
+@section('title', 'Login')
+@section('content')
 
-<style>
+<div class="bloco1">
+    <h1>Bem Vindo</h1>
+     <a href="/">Voltar para a Tela Inicial</a>
+</div>
 
-
-    
-</style>
-
-<body>
-    <div class="bloco1">
-        <h1>Bem Vindo</h1>
-        <a href="/">Voltar para a Tela Inicial</a>
-    </div>
-</body>
-</html>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,10 @@
 @section('title', 'Welcome')
 @section('content')
 
+<header>
+    <a href="login"> Login |</a>
+    <p>Cadastrar</p>
+</header>
 <h1>Hello World</h1>
 
 <!-- Comment -->
